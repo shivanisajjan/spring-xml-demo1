45693637b003f71d8025c6d2371e0c71f996485c
shivanisajjan <shivanisajjan269@gmail.com>
2019-09-25 19:50:40 +0530

    first Commit

--- src/main/java/com/stackroute/MovieConfig.java
+++ src/main/java/com/stackroute/MovieConfig.java
@@ -1,28 +1,12 @@
 package com.stackroute;
 
-import com.stackroute.domain.Actor;
-import com.stackroute.domain.Movie;
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
-@ComponentScan(basePackages = "com.stackroute.domain")
+@ComponentScan(basePackages = "com.stackroute.demo")
 public class MovieConfig {
-    @Bean
-    @Scope("prototype")
-    public Actor actor1(){
-        return new Actor("Shivani",21,"Female");
-    }
-    @Bean
-    @Scope("prototype")
-    public Actor actor2(){
-        return new Actor("Haripriya",21,"Female");
-    }
-    @Bean
-    @Scope("prototype")
-    public Actor actor3(){
-        return new Actor("Deepak",21,"Male");
-    }
+
 }
